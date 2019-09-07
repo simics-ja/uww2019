d4e8ad6dbc06c7632b080fb4ab25330b61a2861e
Shimizu Tomoyuki <smz2develop@gmail.com>
2019-09-07 20:38:30 +0900

    Fix nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+const BASE_URL_SUFFIX = '/uww2019/'
 
 module.exports = {
   mode: 'spa',
@@ -5,17 +6,17 @@ module.exports = {
   ** Headers of the page
   */
   router: {
-    base: '/uww2019/'
+    base: BASE_URL_SUFFIX
   },
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '神戸大学塚本・寺田研究室およびNPO法人ウェアラブルコンピュータ研究開発機構では、ユビキタス・ウェアラブルワークショップを2007年以来毎年開催し、ユビキタス・ウェアラブル技術に関する密度の濃い発表と議論を行っています。' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: BASE_URL_SUFFIX + '/favicon.ico' }
     ]
   },
   /*
