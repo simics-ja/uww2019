0c1b414c0f70eb240bfbc7f384d4390c1ecf346a
Tomoyuki Shimizu <smz2develop@gmail.com>
2019-12-03 17:12:36 +0900

    プログラムページを作成開始

--- pages/program/index.vue
+++ pages/program/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    <h1>テスト</h1>
+  <div class="pt-3 pb-3">
+    <div class="container shadow-sm rounded-lg mb-3">
+      <b-container class="py-3 px-3">
+        <b-row>
+          <h1>テスト</h1>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  computed: {
+    absolutePath () {
+      return `${process.env.baseUrl}${this.$router.history.base}${this.$route.path}`
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
